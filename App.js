e1ae7d2c164830b280a0f7587b2ea2b7bc596809
aviperetz91 <50484941+aviperetz91@users.noreply.github.com>
2019-08-01 16:43:18 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,5 @@
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import AppContainer from './src/Routes'
-
-
 
 class App extends Component{
   render() {
